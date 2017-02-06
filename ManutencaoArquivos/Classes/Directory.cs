9ec85b63458bca633458364c42bfe4ef95c77e76
gabrieelds <gabriel_ads250@hotmail.com>
2017-02-06 14:26:49 -0200

    Add files via upload

---
+++ ManutencaoArquivos/Classes/Directory.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManutencaoArquivos {
+    class Directory {
+        public string[] subDir { get; set; }
+        public string path { get; set; }
+        public string bkpPath { get; set; }
+        public string dayPath { get; set; }
+        string fileBkp { get; set; }
+        string[] fileOriginal = { "vpws.log", "long_qry.dbf", "long_qry.dbt", "gpftrap.log" };
+
+        //Cria o diretório da semana para backup
+        public void createDir(string bkpPath) {
+            if (!System.IO.Directory.Exists(bkpPath)) {
+                System.IO.Directory.CreateDirectory(bkpPath);
+            }
+        }
+
+        //Move os arquivos do fileOriginal e log para o novo diretório
+        public void moveFiles(string path, string bkpPath){
+            foreach(string s in this.fileOriginal){
+                this.fileBkp = bkpPath + @"\" + s;
+                if(File.Exists(path + @"\" + s)){
+                    try {
+                        System.IO.File.Move(path + @"\" + s, this.fileBkp);
+                    } catch (IOException e) {
+                        Console.WriteLine("Erro '{0}'", e);
+                    }
+                }
+            }
+            if (System.IO.Directory.Exists(path + @"\log\")) {
+                try {
+                    System.IO.Directory.Move(path + @"\log\", bkpPath + @"\log\");
+                    System.IO.Directory.CreateDirectory(path + @"\log\");    
+                } catch (IOException e) {
+                    Console.WriteLine("Erro '{0}'", e);
+                }       
+            }
+        }
+
+        //Limpa os arquivos da temp
+        public void cleanTemp(string path) {
+            if (System.IO.Directory.Exists(path + @"\temp\")) {
+                try {
+                    System.IO.Directory.Delete(path + @"\temp\", true);
+                    System.IO.Directory.CreateDirectory(path + @"\temp\");  
+                } catch (IOException e) {
+                    Console.WriteLine("Erro '{0}'", e);
+                }
+            }
+        }
+
+        //Limpa extensões específicas de um diretório
+        public void cleanFolder( string path, string ext) {
+            if (System.IO.Directory.Exists(path)){
+                foreach (string f in System.IO.Directory.GetFiles(path, ext)) {
+                    if(!IsFileLocked(f)){
+                        File.Delete(f);
+                    }
+                }
+            }
+        }
+
+        public bool IsFileLocked(string filePath) {
+            try {
+                using (File.Open(filePath, FileMode.Open)) { }
+            } catch (IOException e) {
+                var errorCode = System.Runtime.InteropServices.Marshal.GetHRForException(e) & ((1 << 16) - 1);
+                return errorCode == 32 || errorCode == 33;
+            }
+            return false;
+        }
+
+    }
+}
+//Cara, o próximo que ler esse código, saiba que estou com preguiça de fazer qualquer outra coisa. Gabriel - 28/07/16 17:45
---
+++ ManutencaoArquivos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManutencaoArquivos {
+    class Program {
+        static void Main(string[] args) {
+            Directory dir = new Directory();
+            dir.path = @"E:\Trucker\";
+            dir.bkpPath = @"C:\Corebuilder\Aplica\_log_BKP\";
+            dir.subDir = System.IO.Directory.GetDirectories(dir.path, "*"); 
+
+            //A pasta de bkp PRECISA se chamar _log_BKP, não to afim de pensar numa forma dinâmica de pegar a última pasta informada na bkpPath
+            foreach (string p in dir.subDir) {
+                if ((p.Split('\\').LastOrDefault() != "_log_BKP") && (p.Split('\\').LastOrDefault() != "Temp")) {
+                    dir.dayPath = dir.bkpPath + p.Split('\\').LastOrDefault() + @"\" + (DateTime.Today.ToString("yyyy-MM-dd"));
+                    dir.createDir(dir.dayPath);
+                    dir.moveFiles(p, dir.dayPath);
+                    dir.cleanTemp(p);
+                    dir.cleanFolder(p, "*.txt");
+                    dir.cleanFolder(p, "*.xml");
+                    dir.cleanFolder(p, "*.sql");
+                    dir.cleanFolder(p, "*.dbf");
+                    dir.cleanFolder(p, "*.dbt");
+                    dir.cleanFolder(p, "*.zip");
+                    dir.cleanFolder(p, "*.log");
+                    dir.cleanFolder(p, "*.csv");
+                }
+            }
+        }
+    }
+}
